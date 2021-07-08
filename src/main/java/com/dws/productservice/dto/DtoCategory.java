3a5af051d3d7be09e6e6a5cacdd6e131a6536a80
Emiliano <galeanaara@ciencias.unam.mx>
2021-07-08 16:59:57 -0500

    create DTO

---
+++ src/main/java/com/dws/productservice/dto/DtoCategory.java
@@ -0,0 +1,39 @@
+package com.productservice.dto;
+
+public class DtoCategory {
+
+    private int id;
+    private String categoria;
+
+    public DtoCategory() {
+        super();
+    }
+
+    public DtoCategory(int id, String categoria) {
+        super();
+        this.id = id;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "DtoCategory [id=" + id + ", categoria=" + categoria + "]";
+    }
+
+}
